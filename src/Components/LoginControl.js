8b5678524d8c533f5c85953945969cd2cc8e080a
Patricio Ernesto Antonio Arena <patricio.e.arena@gmail.com>
2021-10-11 02:05:48 -0300

    refactor: se saca renderizacion de botones para sign in y sign out para metodo de google a metodos en componente LoginControl

--- src/Components/LoginControl.js
+++ src/Components/LoginControl.js
@@ -8,29 +8,39 @@ import StatesOfApplicacion from '../StatesOfApplication'
 const LoginControl = () => {
 
 
-    const { userProfile, removeProfile } = AuthContext()
+    const { userProfile, signInMethod, removeProfile } = AuthContext()
 
     // console.log(userProfile);
 
     const history = useHistory()
 
-    if (userProfile == null) {
-        return (
-            <Link to="/login">
-                <Button className="w-100" onClick={
-                     window.localStorage.setItem(StatesOfApplicacion.signOutGoogle1Key, StatesOfApplicacion.signOutGoogle1Value)
-                     }>Sign In</Button>
-            </Link>
-        )
-    } else {
+
+
+    const renderSignOutGoogleButton = () => {
         return (
             <Link to="/login">
                 <Button className="w-100" onClick={() => {
                     removeProfile();
                     window.localStorage.setItem(StatesOfApplicacion.signOutGoogle2Key, StatesOfApplicacion.signOutGoogle2Value)
-                    }}>Sign Out</Button>
+                }}>Sign Out</Button>
             </Link>
         )
+    }
+
+    const renderSignInGoogleButton = () => {
+        return (
+            <Link to="/login">
+                <Button className="w-100" onClick={
+                    window.localStorage.setItem(StatesOfApplicacion.signOutGoogle1Key, StatesOfApplicacion.signOutGoogle1Value)
+                }>Sign In</Button>
+            </Link>
+        )
+    }
+
+    if (userProfile == null && signInMethod === SignInMethod.Google) {
+        return renderSignInGoogleButton()
+    } else {
+        return renderSignOutGoogleButton()
     }
 
 
